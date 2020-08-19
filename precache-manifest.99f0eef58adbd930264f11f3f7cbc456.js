ca20a6dfba3fb62d30943a7f35fc1ae41084a5df
Shubham Tiwari <Shubham.Tiwari@lntinfotech.com>
2020-08-20 00:27:47 +0530

    Updates

---
+++ precache-manifest.99f0eef58adbd930264f11f3f7cbc456.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f552bef536c9f69a0058d2c78100d5b4",
+    "url": "/collegedunia-clone/index.html"
+  },
+  {
+    "revision": "4fc320ec6de02c025f0a",
+    "url": "/collegedunia-clone/static/css/main.d4c09c47.chunk.css"
+  },
+  {
+    "revision": "22b8891bf340213b3900",
+    "url": "/collegedunia-clone/static/js/2.5ec227e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/collegedunia-clone/static/js/2.5ec227e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fc320ec6de02c025f0a",
+    "url": "/collegedunia-clone/static/js/main.6fc8436f.chunk.js"
+  },
+  {
+    "revision": "c131e06d4ba124db6a08",
+    "url": "/collegedunia-clone/static/js/runtime-main.506fc13a.js"
+  },
+  {
+    "revision": "619ee97eae1c5e93f5d68a9157b2466c",
+    "url": "/collegedunia-clone/static/media/college_02.619ee97e.jpg"
+  }
+]);
